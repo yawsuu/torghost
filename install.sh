a8e822a6bf57f1e5ea3de6b8bc04c37979bb2fa3
Wai Yan Myo Thet <waiyanmyothet@outlook.com>
2019-02-17 02:51:24 +0630

    ported for arch linux

--- install.sh
+++ install.sh
@@ -1,13 +1,15 @@
 clear
 echo "******* Torghost installer ********"
+echo "   [*] Ported for Arch Linux [*] "
+echo "+++++++ FIXED SOME ERROR ++++++++"
 echo ""
 echo "=====> Installing tor bundle "
-sudo apt-get install tor -y -qq
+pacman -Sy tor
 echo "=====> Installing dependencies "
-sudo pip install stem
+pip2 install stem
 echo "=====> Installing TorGhost "
-sudo cp torghost /usr/bin/torghost
-sudo chmod +x /usr/bin/torghost
+cp torghost /usr/bin/torghost
+chmod +x /usr/bin/torghost
 echo "=====> Done "
 echo "=====> Open terminal and type 'torghost' for usage "
 echo "Any questions on cyber security? Join Hacker's QandA forum: https://www.askthehackers.com "
